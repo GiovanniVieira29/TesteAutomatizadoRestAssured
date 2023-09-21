1f1dbc4a986e8ea2e69654cadb104ace80277aec
GiovanniVieira29 <giovanni-lgv@hotmail.com>
2023-09-20 21:47:33 -0300

    first commit

---
+++ TesteAutomatizadoRestAssured/src/test/java/TesteAPI/TesteExemplos.java
@@ -0,0 +1,91 @@
+package TesteAPI;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class TesteExemplos {
+
+	@Test
+	public void ListarUsuarios() {
+
+		Response response = get("https://reqres.in/api/users?page=2");
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+
+		int statusCode = response.getStatusCode();
+
+		Assert.assertEquals(statusCode, 200);
+
+	}
+
+	@Test
+	public void UnicoUsuario() {
+
+		Response response = get("https://reqres.in/api/users/2");
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+
+		int statusCode = response.getStatusCode();
+
+		Assert.assertEquals(statusCode, 200);
+
+	}
+
+	@Test
+	public void ListaRecurso() {
+
+		Response response = get("https://reqres.in/api/unknown");
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+
+		int statusCode = response.getStatusCode();
+
+		Assert.assertEquals(statusCode, 200);
+
+	}
+
+	@Test
+	public void UnicoRecurso() {
+
+		Response response = get("https://reqres.in/api/unknown/2");
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+
+		int statusCode = response.getStatusCode();
+
+		Assert.assertEquals(statusCode, 200);
+
+	}
+
+
+	@Test
+	public void request_2() {
+
+		baseURI = "https://reqres.in/api";
+
+		given().get("users?page=2").then().statusCode(200).body("data[1].id", equalTo(8)).log().all();
+
+	}
+
+}
